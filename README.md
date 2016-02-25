df58ccd754cc2f249a52c95b47cdd16eeb62f82a
tylergoodman <tylergoodman@users.noreply.github.com>
2016-02-25 02:24:08 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Typed rotating-file-stream
+The type definition for [rotating-file-stream](https://github.com/iccicci/rotating-file-stream).
---
+++ rotating-file-stream.d.ts
@@ -0,0 +1,19 @@
+import { EventEmitter } from 'events';
+
+declare function rfs(filename: string | ((time: Date, index: number) => string), options: rfs.RotatingFileStreamOptions): rfs.RotatingFileStream;
+
+declare module rfs {
+  export interface RotatingFileStreamOptions {
+    compress?: string | boolean | ((source: string, dest: string) => string);
+    interval?: string;
+    path?: string;
+    size?: string;
+    highWaterMark?: number;
+    mode?: number;
+  }
+  export interface RotatingFileStream extends EventEmitter {
+    new(filename: string | ((time: Date, index: number) => string), options: RotatingFileStreamOptions): RotatingFileStream;
+  }
+}
+
+export = rfs;
